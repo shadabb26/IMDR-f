fd6ffc34065e5555a26d49f12033c60e3fa32c30
shadabb26 <shadabshikalgar2003@gmail.com>
2024-10-10 18:19:03 +0530

    file structure change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
-import DirectorInfo from "../components/DirectorInfo";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import HeroSection from "../components/HeroSection";
-import MajorRecruiters from "../components/MajorRecruiter";
-import PlacementTeam from "../components/PlacementTeam";
-import RecruitmentProcess from "../components/RecruitmentProcess";
-import TPOInfo from "../components/TPOInfo";
-import WhyIMDR from "../components/WhyIMDR";
+import DirectorInfo from "../components/Home/DirectorInfo";
+import Footer from "../components/Home/Footer";
+import Header from "../components/Home/Header";
+import HeroSection from "../components/Home/HeroSection";
+import MajorRecruiters from "../components/Home/MajorRecruiter";
+import PlacementTeam from "../components/Home/PlacementTeam";
+import RecruitmentProcess from "../components/Home/RecruitmentProcess";
+import TPOInfo from "../components/Home/TPOInfo";
+import WhyIMDR from "../components/Home/WhyIMDR";
 
 export default function Home() {
   return (
